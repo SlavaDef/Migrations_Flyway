3cbd2be4ca9e6bbd2d584f5e9aa558d5075f49b3
SlavaDef <slavadef@gmail.com>
2023-05-27 16:02:25 +0300

    First commit

---
+++ src/main/java/ItGoHomwork/com/App.java
@@ -0,0 +1,13 @@
+package ItGoHomwork.com;
+
+import ItGoHomwork.com.Servise.DataBazeInitServiceV2;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        Database database = Database.getInstance();
+        new DataBazeInitServiceV2().initDB(database);
+    }
+}
---
+++ src/main/java/ItGoHomwork/com/Servise/DataBazeInitServiceV2.java
@@ -0,0 +1,20 @@
+package ItGoHomwork.com.Servise;
+
+import ItGoHomwork.com.Database;
+import org.flywaydb.core.Flyway;
+
+import java.io.IOException;
+
+public class DataBazeInitServiceV2 {
+
+    public static final String DB_URL = "jdbc:h2:C:\\Users\\Vyacheslav\\PROJECTS\\Work_with_migrations/test";
+    // public static final String DB_URL = "jdbc:h2:./test";
+
+    public void initDB(Database database) throws IOException {
+        //String connectionUrl = ReadingServise.readSQL(DB_URL);
+        Flyway flyway = Flyway.configure().
+                dataSource(DB_URL, null, null)
+                .load();
+        flyway.migrate();
+    }
+}
